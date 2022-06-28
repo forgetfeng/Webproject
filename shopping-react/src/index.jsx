728eefda689a90c74eddd121a75d147d7b9c45fd
huangyafeng <alone1346589275@163.com>
2022-06-29 00:10:50 +0800

    提交

--- shopping-react/src/index.jsx
+++ shopping-react/src/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
 import reactDom from "react-dom"
 import App from "./App"
+import 'antd-mobile/dist/antd-mobile.css';  
 reactDom.render(<App />, document.getElementById('root'))
